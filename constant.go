9084843d4057b96b7c12319a21f90e8fa749e1ab
imronarth <imron@gmx.com>
2024-01-31 22:04:38 +0700

    Belajar Constant

---
+++ constant.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+	const firstName string = "Imron"
+	const lastName = "Athoriq"
+	const(
+		satuNama = "Imron"
+		duaNama = "Imron Athoriq"
+	)
+	fmt.Println(satuNama, duaNama)
+}
