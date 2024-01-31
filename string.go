02eead5678e77c4ca9fea29efdd2b95e0cefa689
imronarth <imron@gmx.com>
2024-01-31 21:52:07 +0700

    Belajar string

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Imron")
+	fmt.Println("Imron Athoriq")
+	fmt.Println("Imron Athoriq Firjatulloh")
+
+	fmt.Println(len("Imron"))
+	fmt.Println("Imron Athoriq"[0])
+	fmt.Println("Imron Athoriq Firjatulloh"[2])
+}
